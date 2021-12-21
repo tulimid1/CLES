82d9d840e170d04dc54c74bc22bf8180f98568e8
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 09:12:05 -0500

    update files for pypi

--- CLES/CLES.py
+++ CLES/CLES.py
@@ -1,5 +1,43 @@
-def CLES_2independent():
-    pass 
+def CLES_2independent(data1, data2):
+    '''
+    Calculate common language effect size (2 samples)
 
-def CLES_2paired():
-    pass 
+    INPUTS:
+    data1/2: data set, np.array
+
+    OUTPUT:
+    thetaHat: common language effect size (50 - 100)
+    '''
+    import numpy 
+    total = 0
+    for d1 in data1:
+        for d2 in data2:
+            if d1 > d2: 
+                total += 1
+            elif d1 == d2: 
+                total += 0.5
+    thetaHat = (numpy.abs(total / (len(data1) * len(data2)) - 0.5) + 0.5) * 100
+    return thetaHat 
+
+def CLES_2paired(data1, data2):
+    '''
+    Calculate common language effect size (paired samples)
+
+    INPUTS:
+    data1/2: data set, np.array
+
+    OUTPUT:
+    thetaHat: common language effect size (50 - 100)
+    '''
+    import numpy 
+    D = data1 - data2
+    comp = numpy.zeros((len(D)-1))
+    total = 0
+    for d1 in D:
+        for d2 in comp:
+            if d1 > d2: 
+                total += 1
+            elif d1 == d2: 
+                total += 0.5
+    thetaHat = (numpy.abs(total / (len(data1) * len(data2)) - 0.5) + 0.5) * 100
+    return thetaHat 
