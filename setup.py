ae28d729c3682a2c0d28840cd5e889c4f6285707
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 09:35:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'CLES',         # How you named your package folder (MyLib)
   packages = ['CLES'],   # Chose the same as "name"
-  version = '0.1.1',      # Start with a small number and increase it with every change you make
+  version = '0.1.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Calculate common language effect size (CLES) for 2 paired or independent samples',   # Give a short description about your library
   author = 'Duncan Tulimieri',                   # Type in your name
   author_email = 'tulimid@udel.edu',      # Type in your E-Mail
   url = 'https://tulimid1.github.io/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tulimid1/CLES/archive/refs/tags/v_01.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tulimid1/CLES/archive/refs/tags/v_01.2.tar.gz',    # I explain this later on
   keywords = ['common', 'language', 'effect', 'size'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
