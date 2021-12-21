ff010a81a3ed54671bb427db8afb14e4abf43b62
Duncan Tulimieri <tulimid@udel.edu>
2021-12-21 09:24:07 -0500

    add 1 sample

--- CLES/CLES.py
+++ CLES/CLES.py
@@ -41,3 +41,26 @@ def CLES_2paired(data1, data2):
                 total += 0.5
     thetaHat = (numpy.abs(total / (len(data1) * len(data2)) - 0.5) + 0.5) * 100
     return thetaHat 
+
+def CLES_1samp(data, mu=0):
+    '''
+    Calculate common language effect size (1 sample)
+
+    INPUTS:
+    data: data vector 
+    mu: mean to compare, default=0
+
+    OUTPUT:
+    thetaHat: common language effect size (50 - 100)
+    '''
+    import numpy 
+    comp = numpy.repeat(mu, len(data)) # mean value to compare 
+    total = 0
+    for d1 in data:
+        for d2 in comp:
+            if d1 > d2: 
+                total += 1
+            elif d1 == d2: 
+                total += 0.5
+    thetaHat = (numpy.abs(total / (len(data) * len(comp)) - 0.5) + 0.5) * 100
+    return thetaHat
